2d06702706dc9912542cb195b06cc52a92a75513
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-21 21:10:36 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,10 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
+class Kid
   include Dance
 
   attr_accessor :name
